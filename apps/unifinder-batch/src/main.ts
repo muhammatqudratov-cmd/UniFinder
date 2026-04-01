72ddc7b700a472296fca182286639e4268f02c54
Mukhammed <muhammatqudratov@gmail.com>
2026-04-01 19:50:12 +0900

    feat: configure env variables

--- apps/unifinder-batch/src/main.ts
+++ apps/unifinder-batch/src/main.ts
@@ -3,6 +3,6 @@ import { UnifinderBatchModule } from './unifinder-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(UnifinderBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
--- apps/unifinder-batch/src/unifinder-batch.module.ts
+++ apps/unifinder-batch/src/unifinder-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UnifinderBatchController } from './unifinder-batch.controller';
 import { UnifinderBatchService } from './unifinder-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [UnifinderBatchController],
   providers: [UnifinderBatchService],
 })
