c6d8619aa01d19dfae1a5b8c24a49f8f4623d17f
Mukhammed <muhammatqudratov@gmail.com>
2026-04-01 20:12:36 +0900

    feat: integration GraphQLModule and build first GraphQL Api

--- apps/unifinder-api/src/app.service.ts
+++ apps/unifinder-api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'UNI-FINDER API is running!';
+    return 'UNI-FINDER REST API is running!';
   }
 }
