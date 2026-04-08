42a307713f95f29d315fa6999fcda0b097f6ea0d
Mukhammed <muhammatqudratov@gmail.com>
2026-04-08 20:49:27 +0900

    feat: develop Global Error handling

--- apps/unifinder-api/src/app.module.ts
+++ apps/unifinder-api/src/app.module.ts
@@ -7,18 +7,30 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
-
-
-
+import { T } from './libs/types/common';
 
 @Module({
-  imports: [ConfigModule.forRoot(), GraphQLModule.forRoot({
-    		driver: ApolloDriver,
-      playground: true,
-      uploads: false,
-      autoSchemaFile: true,
-  }), ComponentsModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService, AppResolver],
+	imports: [
+		ConfigModule.forRoot(),
+		GraphQLModule.forRoot({
+			driver: ApolloDriver,
+			playground: true,
+			uploads: false,
+			autoSchemaFile: true,
+			formatError: (error: T) => {
+				const graphQLFormattedError = {
+					code: error?.extensions.code,
+					message:
+						error?.extensions?.exception?.response?.message || error?.extensions?.response?.message || error?.message,
+				};
+				console.log('GRAPHQL GLOBAL ERR:', graphQLFormattedError);
+				return graphQLFormattedError;
+			},
+		}),
+		ComponentsModule,
+		DatabaseModule,
+	],
+	controllers: [AppController],
+	providers: [AppService, AppResolver],
 })
 export class AppModule {}
--- apps/unifinder-api/src/components/member/member.resolver.ts
+++ apps/unifinder-api/src/components/member/member.resolver.ts
@@ -1,4 +1,4 @@
-import { InternalServerErrorException, UsePipes, ValidationPipe } from '@nestjs/common';
+import { InternalServerErrorException } from '@nestjs/common';
 import { MemberService } from './member.service';
 import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
@@ -10,24 +10,14 @@ export class MemberResolver {
 
 	@Mutation(() => Member)
 	public async signup(@Args('input') input: MemberInput): Promise<Member> {
-		try {
-			console.log('Mutation: signup');
-			return this.memberService.signup(input);
-		} catch (err) {
-			console.log('Error, signup:', err);
-			throw new InternalServerErrorException(err);
-		}
+		console.log('Mutation: signup');
+		return this.memberService.signup(input);
 	}
 
 	@Mutation(() => Member)
 	public async login(@Args('input') input: LoginInput): Promise<Member> {
-		try {
-			console.log('Mutation: login');
-			return this.memberService.login(input);
-		} catch (err) {
-			console.log('Error, login:', err);
-			throw new InternalServerErrorException(err);
-		}
+		console.log('Mutation: login');
+		return this.memberService.login(input);
 	}
 
 	@Mutation(() => String)
