3657459fa81dcac75a0a76ac31e162a7a64a1d3e
Mukhammed <muhammatqudratov@gmail.com>
2026-05-04 19:27:15 +0900

    feat: modify BatchModule and connected MongoDB

--- apps/unifinder-batch/src/unifinder-batch.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class UnifinderBatchService {
-  getHello(): string {
-    return 'UNI-FINDER Batch is running!';
-  }
-}
--- apps/unifinder-batch/test/app.e2e-spec.ts
+++ apps/unifinder-batch/test/app.e2e-spec.ts
@@ -1,24 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { UnifinderBatchModule } from './../src/unifinder-batch.module';
+import { UnifinderBatchModule } from '../src/batch.module';
 
 describe('UnifinderBatchController (e2e)', () => {
-  let app: INestApplication;
+	let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [UnifinderBatchModule],
-    }).compile();
+	beforeEach(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [UnifinderBatchModule],
+		}).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+		app = moduleFixture.createNestApplication();
+		await app.init();
+	});
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+	it('/ (GET)', () => {
+		return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+	});
 });
